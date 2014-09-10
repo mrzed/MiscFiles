5ef3f2b7dfda94f7251a954a872ad4ed32a9d26f
Devin Trotter <dtrotter@gmail.com>
2014-09-10 16:03:00 -0600

    my lazy qemu script

---
+++ my_lb_scripts/qemu.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# I am lazy and didn't want to keep typing this, so I wrote a script. Oogity boogity boo.
+# Devin Trotter (Zed)
+# zed@lilypadmudlib.com
+
+if [ $1 == "--help" ]; then
+    echo ""
+    echo "Help!"
+    echo ""
+    echo "USAGE:"
+    echo ""
+    echo ""
+    echo "./qemu.sh <filename.iso>"
+    echo ""
+    exit 2
+fi
+
+if [ $1 == "" ]; then
+     echo ""
+    echo "Help!"
+    echo ""
+    echo "Need to add stuff here."
+    echo ""
+    exit 2
+fi
+
+if [[ ( $1 != "--help" && $1 != "" ) ]]; then
+    clear
+    qemu-system-x86_64 -m 256 $1
+    exit 2
+fi
