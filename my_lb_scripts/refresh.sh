640c7f4bca8485c37764afaba029b3f47a1d9f04
Devin Trotter <dtrotter@gmail.com>
2014-09-10 14:58:26 -0600

    add a few lines to refresh.sh

--- my_lb_scripts/refresh.sh
+++ my_lb_scripts/refresh.sh
@@ -9,12 +9,14 @@ clear
 # Verify it is executed as root
 if [ ! $UID -eq 0 ]; then
     echo "This script must be run as root."
+    echo " "
     exit 1
 fi
 
 # Execute 'lb clean'
 if [ -z $1 ]; then
     echo "Cleaning your build directory..."
+    echo " "
     lb clean
     exit 2
 fi
@@ -22,6 +24,7 @@ fi
 # Execute 'lb clean' and then 'lb clean --purge'
 if [ $1 == "--all" ]; then
     echo "Cleaning and purging your build directory..."
+    echo " "
     lb clean ; cl ; lb clean --purge
     exit 2
 fi
@@ -29,6 +32,7 @@ fi
 # Execute 'lb clean', clear the screen, execute 'lb clean --purge', then 'lb build'
 if [ $1 == "--ball" ]; then
     echo "Cleaning and purging your build directory, then rebuilding..."
+    echo " "
     lb clean ; cl ; lb clean --purge ; lb build
     exit 2
 fi
